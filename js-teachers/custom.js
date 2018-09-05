f28b19350b16401b059de83b802fede415dab570
Vu Tony <vutran165@gmail.com>
2018-09-05 21:53:54 +0700

    1-05092018

--- js-teachers/custom.js
+++ js-teachers/custom.js
@@ -1,7 +1,21 @@
 (function ($) {
     "use strict";
 
+    // pre-loader
+  
+
     //home slider & courses & clients
+
+    $('.home-slider').owlCarousel({
+        animateOut: 'fadeOut',
+        items: 1,
+        loop: true,
+        dots: false,
+        autoplayHoverPause: false,
+        autoplay: true,
+        smartSpeed: 1000
+    })
+
     $('.owl-courses').owlCarousel({
         animateOut: 'fadeOut',
         loop: true,
@@ -13,13 +27,13 @@
             '<i class="fa fa-angle-left"></i>',
             '<i class="fa fa-angle-right"></i>'
         ],
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,                
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
             },
-            1000:{
-                items:3,
+            1000: {
+                items: 3,
             }
         }
 
